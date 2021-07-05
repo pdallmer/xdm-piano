be2f02e4e3d9a2523260558c8a36fdb66520f3e2
pdallmer <philipdallmer@googlemail.com>
2021-07-05 15:03:42 +0200

    allpass filter in delay loop

--- src/piano_string.c
+++ src/piano_string.c
@@ -2,20 +2,34 @@
 
 void initialize_string(string* s, float frequency, int sample_rate)
 {
-	s->frequency = frequency;	
+	s->frequency = frequency;
 	s->delay_line_length = ceil((float)sample_rate / (float)frequency);
 	s->delay_line = (float*)malloc(s->delay_line_length * sizeof(float));
 	s->state = NOTE_OFF;	
-	s->two_pole = (float*)malloc(2 * sizeof(float));
+	s->biquad = (float*)malloc(2 * sizeof(float));
 }
 
 void get_string_samples(float* buffer, string* s, int n_samples)
 {
-	int next_index;
+	float g, b1, b2, a1, a2, r;
+	r = 0.9;
+	a2 = r * r;
+	g = a2;
+	a1 = -2 * r * cosf(2 * M_PI * s->frequency);
+	b1 = a1 / a2;
+	b2 = 1 / a2;
 	for(int i = 0; i < n_samples; i++)
 	{
 		//output
 		buffer[i] += s->delay_line[s->delay_line_out];
+		//dispersion
+		s->delay_line[s->delay_line_out] = g * s->delay_line[s->delay_line_out] 
+											+ b1 * g * s->delay_line[(s->delay_line_out + 1) % s->delay_line_length] 
+											+ b2 * g * s->delay_line[(s->delay_line_out + 2) % s->delay_line_length] 
+											- a1 * s->biquad[(s->biquad_out + 1) % 2] 
+											- a2 * s->biquad[s->biquad_out];
+		s->biquad[s->biquad_out] = s->delay_line[s->delay_line_out];
+		s->biquad_out = (s->biquad_out + 1) % 2;
 		//damping
 		s->delay_line[s->delay_line_out] = 0.5 * s->delay_line[s->delay_line_out] + 0.5 * s->delay_line[(s->delay_line_out + 1) % s->delay_line_length];
 		//increase counter
@@ -28,12 +42,10 @@ void excite_string(string* s, int velocity)
 	s->state = NOTE_ON;
 	float v = (float)velocity / 128.0;
 	s->delay_line_out = 0;
-	s->two_pole_out = 0;
+	s->biquad_out = 0;
 	for(int i = 0; i < s->delay_line_length; i++)
 	{
-		s->delay_line[i] =  0.2 * random_float(v) + 2 * 0.9 * cosf(440) * s->two_pole[(s->two_pole_out + 1) % 2] - 0.89 * s->two_pole[s->two_pole_out];
-		s->two_pole[s->two_pole_out] = s->delay_line[i];
-		s->two_pole_out = (s->two_pole_out + 1) % 2;
+		s->delay_line[i] =  random_float(v);
 	}	
 }
 void stop_string(string* s)
--- src/piano_string.h
+++ src/piano_string.h
@@ -21,9 +21,9 @@ struct string
 	float frequency;
 	int delay_line_length; 
 	int delay_line_out;
-	int two_pole_out;
-	float* two_pole;
+	int biquad_out;
 	float* delay_line;
+	float* biquad;
 	enum string_state state;
 };
 
