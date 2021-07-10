add9a86882fd9f4387adb0f2a118a796caa0f5ed
pdallmer <philipdallmer@googlemail.com>
2021-07-10 10:54:35 +0200

    removed allpass filter & filter excitation noise with time varying lowpass filter

--- src/piano_string.c
+++ src/piano_string.c
@@ -12,24 +12,13 @@ void get_string_samples(float* buffer, string* s, int n_samples)
 {
 	float g, b1, b2, a1, a2, r;
 
-	r = 0.9;
-	a2 = r * r;
-	g = a2;
-	a1 = -2 * r * cosf(2 * M_PI * 440);
-	b1 = a1 / a2;
-	b2 = 1 / a2;
+	
 	for(int i = 0; i < n_samples; i++)
 	{
 		//output
-		buffer[i] += s->delay_line[s->delay_line_out];
-		//dispersion
-		s->delay_line[s->delay_line_out] = g * s->delay_line[s->delay_line_out] 
-											+ b1 * g * s->delay_line[(s->delay_line_out + 1) % s->delay_line_length] 
-											+ b2 * g * s->delay_line[(s->delay_line_out + 2) % s->delay_line_length] 
-											- a1 * s->delay_line[(s->delay_line_out -1) % s->delay_line_length] 
-											- a2 * s->delay_line[(s->delay_line_out - 2) % s->delay_line_length];
+		buffer[i] += s->delay_line[s->delay_line_out];	
 		//damping
-		s->delay_line[s->delay_line_out] = 0.3 * s->delay_line[s->delay_line_out] + 0.7 * s->delay_line[(s->delay_line_out + 1) % s->delay_line_length];
+		s->delay_line[s->delay_line_out] = 0.5 * s->delay_line[s->delay_line_out] + 0.5 * s->delay_line[(s->delay_line_out + 1) % s->delay_line_length];
 		//increase counter
 		s->delay_line_out = (s->delay_line_out + 1) % s->delay_line_length;
 	}
@@ -40,9 +29,15 @@ void excite_string(string* s, int velocity)
 	s->state = NOTE_ON;
 	float v = (float)velocity / 128.0;
 	s->delay_line_out = 0;
+	float a, f, feedback;
 	for(int i = 0; i < s->delay_line_length; i++)
 	{
 		s->delay_line[i] =  0.1 * random_float(v) + 0.9 * s->delay_line[(i - 1) % s->delay_line_length];
+		//filter excitation input
+		f = 20000 + ( -i * 20000 ) / (s->delay_line_length - 1);
+		a = f / (1 + f);
+		feedback = i == 0 ? 0 : s->delay_line[i - 1];
+		s->delay_line[i] = a * s->delay_line[i] + (1 - a) * feedback;
 	}	
 }
 void stop_string(string* s)
